79ba6f9206319e0d88227aea7b21901a4fd6c6ee
Jim Plimmer <jim123abc3@gmail.com>
2022-10-16 18:59:11 +0100

    initial commit

---
+++ js_modules/src/image_viewer.js
@@ -0,0 +1,6 @@
+import '../styles/image_viewer.css';
+
+const image = document.createElement('img');
+image.src = 'https://picsum.photos/200'
+
+document.body.appendChild(image);
---
+++ js_modules/src/index.js
@@ -0,0 +1,6 @@
+import sum from './sum';
+import './image_viewer.js';
+
+const total = sum(10,5);
+
+console.log(total);
